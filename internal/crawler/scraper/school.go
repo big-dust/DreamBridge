36ccc2fb807f3fdc01b029bbbe5000a57a25c737
bigdust <3264085417@qq.com>
2024-02-02 15:08:03 +0800

    feat: 添加爬取学校专业信息功能

--- internal/crawler/scraper/school.go
+++ internal/crawler/scraper/school.go
@@ -103,7 +103,7 @@ func JobDetail(schoolId int) (*model.JobDetailResponse, error) {
 	return jobDetailResp, nil
 }
 
-func NationFeature(schoolId int) (*model.NationFeatureResponse, error) {
+func SpecialInfo(schoolId int) (*model.SpecialResponse, error) {
 	client := &http.Client{}
 	url := fmt.Sprintf("https://static-data.gaokao.cn/www/2.0/school/%d/pc_special.json", schoolId)
 	req, err := http.NewRequest("GET", url, nil)
@@ -125,9 +125,68 @@ func NationFeature(schoolId int) (*model.NationFeatureResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-	nationFeatureResp := &model.NationFeatureResponse{}
+	nationFeatureResp := &model.SpecialResponse{}
 	if err = json.Unmarshal(bodyText, nationFeatureResp); err != nil {
 		return nil, err
 	}
 	return nationFeatureResp, nil
+}
+
+// 湖北locationId = 42
+func HistoryRecruit(schoolId int, locationId int) (*model.HistoryRecruitResponse, error) {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://static-data.gaokao.cn/www/2.0/history_recruit/%d/%d.json", schoolId, locationId)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("sec-ch-ua", `"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"`)
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Referer", "https://www.gaokao.cn/")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36")
+	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	historyRecruitResp := &model.HistoryRecruitResponse{}
+	if err := json.Unmarshal(bodyText, historyRecruitResp); err != nil {
+		return nil, err
+	}
+	return historyRecruitResp, nil
+}
+
+func HistoryAdmission(schoolId int, locationId int) (*model.HistoryAdmissionResponse, error) {
+	client := &http.Client{}
+	url := fmt.Sprintf("https://static-data.gaokao.cn/www/2.0/history_admission/%d/%d.json", schoolId, locationId)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("sec-ch-ua", `"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"`)
+	req.Header.Set("Accept", "application/json, text/plain, */*")
+	req.Header.Set("Referer", "https://www.gaokao.cn/")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36")
+	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	historyAdmissionResp := &model.HistoryAdmissionResponse{}
+	if err = json.Unmarshal(bodyText, historyAdmissionResp); err != nil {
+		return nil, err
+	}
+	return historyAdmissionResp, nil
 }
--- script/db/init.sql
+++ script/db/init.sql
@@ -35,14 +35,7 @@ create table scores (
 
 create table majors (
                         id int auto_increment primary key, -- 主键ID
-                        year     int,
-                        location varchar(50),
-                        highest_score int, -- 最高分
-                        lowest_score int, -- 最低分
-                        highest_rank int, -- 最高名次
-                        lowest_rank int, -- 最低名次
                         name varchar(255) not null, -- 名称
-                        recruitment_number int, -- 招收人数
                         national_feature boolean, -- 国家特色
                         level varchar(100), -- 层次
                         discipline_category varchar(100), -- 学科门类
@@ -50,4 +43,16 @@ create table majors (
                         duration int, -- 学制
                         school_id int, -- 所属院校ID
                         foreign key (school_id) references schools(id) -- 外键关联院校表
+);
+
+create table scores (
+                        special_id int,
+                        location varchar(50),
+                        year     int,
+                        kelei    varchar(50),
+                        recruitment_number int, -- 招收人数
+                        highest_score int, -- 最高分
+                        lowest_score int, -- 最低分
+                        highest_rank int, -- 最高名次
+                        lowest_rank int -- 最低名次
 );
